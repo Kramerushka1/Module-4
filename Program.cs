9a6e6c9b2432ecb4a0a9e1bda0e33782557fb794
Kramerushka <email@example.rupainandlove95@gmail.com>
2023-10-25 11:36:09 +0600

    Задание 4.4.5

--- Program.cs
+++ Program.cs
@@ -7,20 +7,22 @@ namespace Module_4
     {
         public static void Main(string[] args)
         {
-            var (name, age) = ("Виктор", 28);
-            Console.WriteLine("Мое имя: {0}", name);
-            Console.WriteLine("Мой возраст: {0}", age);
+            (string Name, string Type, double Age, int NameCount) Pet;
 
+            Console.Write("Укажите имя питомца: ");
+            Pet.Name = Console.ReadLine();
+            Console.Write("Укажите тип питомца: ");
+            Pet.Type = Console.ReadLine();
+            Console.Write("Укажите возраст питомца: ");
+            Pet.Age = double.Parse(Console.ReadLine());
+            Pet.NameCount = Pet.Name.Length;
+            
             Console.WriteLine();
 
-            Console.Write("Введите ваше имя: ");
-            name = Console.ReadLine();
-            Console.Write("Введите ваш возраст: ");
-            age = int.Parse(Console.ReadLine());
-
-            Console.WriteLine();
-            Console.WriteLine("Ваше имя: {0}", name);
-            Console.WriteLine("Ваш возраст: {0}", age);
+            Console.WriteLine("Имя питомца: {0}", Pet.Name);
+            Console.WriteLine("Тип питомца: {0}", Pet.Type);
+            Console.WriteLine("Возраст питомца: {0}", Pet.Age);
+            Console.WriteLine("Длина имени питомца: {0}", Pet.NameCount);
         }
     }
 }
